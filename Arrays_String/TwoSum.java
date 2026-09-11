ba7d5a8f7ae81244f00ceed1e1dfd90c838638d7
Satyapriya Paikaray <satyapriyap67@gmail.com>
2026-09-11 23:40:47 +0530

    TwoSum Of Array

---
+++ Arrays_String/TwoSum.java
@@ -0,0 +1,31 @@
+package Arrays_String;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Arrays;
+
+public class TwoSum {
+    
+    public static int[] findTwoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+    
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+            
+            map.put(nums[i], i);
+        }
+        
+        throw new IllegalArgumentException("No two sum solution found");
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        
+        int[] result = findTwoSum(nums, target);
+        System.out.println("Indices: " + Arrays.toString(result));
+    }
+}
