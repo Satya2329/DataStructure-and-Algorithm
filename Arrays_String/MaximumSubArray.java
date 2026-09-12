20ae0b4024bc860c7575da80e94223e5483a0b45
Satyapriya Paikaray <satyapriyap67@gmail.com>
2026-09-12 09:11:54 +0530

    MaximumSubArray

---
+++ Arrays_String/MaximumSubArray.java
@@ -0,0 +1,22 @@
+package Arrays_String;
+public class MaximumSubArray {
+    public int maxSubArray(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int currentSum = nums[0];
+        int maxSum = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            currentSum = Math.max(nums[i], currentSum + nums[i]);
+            maxSum = Math.max(maxSum, currentSum);
+        }
+        return maxSum;
+    }
+    public static void main(String[] args) {
+        MaximumSubArray solver = new MaximumSubArray();
+        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        
+        int result = solver.maxSubArray(nums);
+        System.out.println("Maximum subarray sum: " + result);
+    }
+}
