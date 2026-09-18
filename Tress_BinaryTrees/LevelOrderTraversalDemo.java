0281299f26c3ef27affb0fcfbf821b7debf21bd3
Satyapriya Paikaray <satyapriyap67@gmail.com>
2026-09-18 23:36:24 +0530

    LevelOrderTraversal

---
+++ Tress_BinaryTrees/LevelOrderTraversalDemo.java
@@ -0,0 +1,69 @@
+package Tress_BinaryTrees;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+// Definition for a binary tree node.
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    
+    TreeNode(int val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+public class LevelOrderTraversalDemo {
+    
+    // Level Order Traversal Method
+    public static List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            List<Integer> currentLevel = new ArrayList<>();
+            
+            // Process all nodes at the current level
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode currentNode = queue.poll();
+                currentLevel.add(currentNode.val);
+                
+                if (currentNode.left != null) {
+                    queue.offer(currentNode.left);
+                }
+                if (currentNode.right != null) {
+                    queue.offer(currentNode.right);
+                }
+            }
+            result.add(currentLevel);
+        }
+        
+        return result;
+    }
+    
+    public static void main(String[] args) {
+
+        
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+        
+        List<List<Integer>> result = levelOrder(root);
+        
+        System.out.println("Level Order Traversal Result:");
+        System.out.println(result);
+    }
+}
