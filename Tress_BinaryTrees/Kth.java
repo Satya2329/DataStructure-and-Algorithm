ac0937a74618b6082cf0c5548e912df4e096aa57
Satyapriya Paikaray <satyapriyap67@gmail.com>
2026-09-22 23:15:22 +0530

    Kth smallest element in Binary Search Tree

---
+++ Tress_BinaryTrees/Kth.java
@@ -0,0 +1,49 @@
+package Tress_BinaryTrees;
+import java.util.Stack;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+}
+
+public class Kth{
+    public static int kthSmallest(TreeNode root, int k) {
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode curr = root;
+
+        while (curr != null || !stack.isEmpty()) {
+            while (curr != null) {
+                stack.push(curr);
+                curr = curr.left;
+            }
+
+            curr = stack.pop();
+            k--;
+
+            if (k == 0) {
+                return curr.val;
+            }
+
+            curr = curr.right;
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(5);
+        root.left = new TreeNode(3);
+        root.right = new TreeNode(6);
+        root.left.left = new TreeNode(2);
+        root.left.right = new TreeNode(4);
+        root.left.left.left = new TreeNode(1);
+
+        int k = 3;
+        System.out.println(kthSmallest(root, k));
+    }
+}
